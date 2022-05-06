72f8590b7098b27c604b5708adcd574f10b1dc6c
Hank Yu <hank.yu@belkin.com>
2022-05-06 12:52:22 +0800

    [THEME] Remove the playground pages from the main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:moab_poc/design/themes.dart';
-import 'package:moab_poc/page/playground/entry_page.dart';
-import 'package:moab_poc/page/playground/setup_page_1.dart';
-import 'package:moab_poc/page/playground/setup_page_2.dart';
 import 'package:moab_poc/page/dashboard/view.dart';
 import 'package:moab_poc/page/landing_page/view.dart';
 import 'package:moab_poc/page/login/view.dart';
@@ -18,15 +15,12 @@ class MoabApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: EntryPage.routeName,
+      initialRoute: LandingPage.routeName,
       routes: {
-        EntryPage.routeName: (context) => const EntryPage(),
-        SetupPage1.routeName: (context) => const SetupPage1(),
-        SetupPage2.routeName: (context) => const SetupPage2(),
-        // LandingPage.routeName: (context) => const LandingPage(),
-        // LoginPage.routeName: (context) => const LoginPage(),
-        // DashboardPage.routeName: (context) => const DashboardPage(),
-        // MeshPage.routeName: (context) => const MeshPage(),
+        LandingPage.routeName: (context) => const LandingPage(),
+        LoginPage.routeName: (context) => const LoginPage(),
+        DashboardPage.routeName: (context) => const DashboardPage(),
+        MeshPage.routeName: (context) => const MeshPage(),
       },
       theme: MoabTheme.mainLightModeData,
       darkTheme: MoabTheme.mainDarkModeData,
