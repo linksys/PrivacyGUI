9e7be5ae31bba64e61d2a7b1315067899214f5bc
Austin Chang <austin.chang@belkin.com>
2022-05-31 16:31:59 +0800

    Add firebase framework

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,42 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:moab_poc/design/themes.dart';
 import 'package:moab_poc/route/route.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'firebase_options.dart';
 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
 
-void main() {
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+
+  print('User granted permission: ${settings.authorizationStatus}');
+  String? token = await FirebaseMessaging.instance.getToken();
+  String? apnsToken = await FirebaseMessaging.instance.getAPNSToken();
+  print('Token:: FCM:${token ?? ''}, APNS:${apnsToken ?? ''}');
+
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.data}');
+    print('Message notification: ${message.notification}');
+
+    if (message.notification != null) {
+      print('Message also contained a notification: ${message.notification}');
+    }
+  });
   runApp(const NavigatorDemo());
 }
 
