5cada6bda4bc1d05f8898551d2b69908d1743866
matt.yang <matt.yang@belkin.com>
2022-06-08 15:58:35 +0800

    Migrate qr_code_scanner to mobile_scanner as new lib use latest API and old lib switch to maintain and dependencies are deprecated.

--- lib/page/components/qr_view.dart
+++ lib/page/components/qr_view.dart
@@ -1,25 +1,17 @@
 import 'dart:developer';
-import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 
 typedef OnResultScanned = Function(Barcode barcode);
-typedef OnScannedFinish = Function(Barcode barcode);
 
 class CustomQRView extends StatefulWidget {
-  final OnResultScanned? callback;
-  final OnScannedFinish? onFinish;
-  final QrScannerOverlayShape? overlay;
-  final EdgeInsetsGeometry overlayMargin;
+  final OnResultScanned? onResult;
 
-  const CustomQRView(
-      {Key? key,
-      this.callback,
-      this.onFinish,
-      this.overlay,
-      this.overlayMargin = EdgeInsets.zero})
-      : super(key: key);
+  const CustomQRView({
+    Key? key,
+    this.onResult,
+  }) : super(key: key);
 
   @override
   State<StatefulWidget> createState() => _QRViewState();
@@ -27,19 +19,9 @@ class CustomQRView extends StatefulWidget {
 
 class _QRViewState extends State<CustomQRView> {
   Barcode? result;
-  QRViewController? controller;
-  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
-
-  // In order to get hot reload to work we need to pause the camera if the platform
-  // is android, or resume the camera if the platform is iOS.
-  @override
-  void reassemble() {
-    super.reassemble();
-    if (Platform.isAndroid) {
-      controller!.pauseCamera();
-    }
-    controller!.resumeCamera();
-  }
+  MobileScannerController? controller =
+      MobileScannerController(facing: CameraFacing.front, torchEnabled: true);
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QRCode');
 
   @override
   Widget build(BuildContext context) {
@@ -55,45 +37,23 @@ class _QRViewState extends State<CustomQRView> {
   Widget _buildQrView(BuildContext context) {
     // To ensure the Scanner view is properly sizes after rotation
     // we need to listen for Flutter SizeChanged notification and update controller
-    return QRView(
-      key: qrKey,
-      onQRViewCreated: _onQRViewCreated,
-      overlay: widget.overlay,
-      onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),
-    );
-  }
-
-  void _onQRViewCreated(QRViewController controller) {
-    setState(() {
-      this.controller = controller;
-    });
-    controller.scannedDataStream.listen((scanData) {
-      if (result == null) {
-        setState(() {
-          result = scanData;
+    // return QRView(
+    //   key: qrKey,
+    //   onQRViewCreated: _onQRViewCreated,
+    //   overlay: widget.overlay,
+    //   onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),
+    // );
+    return MobileScanner(
+        allowDuplicates: false,
+        controller: controller,
+        onDetect: (barcode, args) {
+          if (barcode.rawValue == null) {
+            debugPrint('Failed to scan Barcode');
+          } else {
+            debugPrint('Barcode found: ${barcode.toString()}');
+          }
+          widget.onResult?.call(barcode);
         });
-        widget.callback?.call(result!);
-        // Timer(1.seconds, () {
-        //   setState(() {
-        //     result = null;
-        //   });
-        // });
-        controller.pauseCamera();
-        controller.dispose();
-      }
-    }, onDone: () {
-      log("QR: onDone");
-      widget.onFinish?.call(result!);
-    });
-  }
-
-  void _onPermissionSet(BuildContext context, QRViewController ctrl, bool p) {
-    log('${DateTime.now().toIso8601String()}_onPermissionSet $p');
-    if (!p) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('no Permission')),
-      );
-    }
   }
 
   @override
--- lib/page/setup/parent_scan_qrcode_view.dart
+++ lib/page/setup/parent_scan_qrcode_view.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:moab_poc/design/colors.dart';
 import 'package:moab_poc/page/components/base_components/base_page_view.dart';
 import 'package:moab_poc/page/components/layouts/basic_header.dart';
 import 'package:moab_poc/page/components/layouts/basic_layout.dart';
 import 'package:moab_poc/page/components/qr_view.dart';
-import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 
 import '../components/base_components/button/primary_button.dart';
 import '../components/base_components/button/simple_text_button.dart';
@@ -28,16 +27,17 @@ class ParentScanQRCodeView extends StatelessWidget {
         content: Center(
           child: Stack(
             children: [
-              CustomQRView(
-                callback: (Barcode code) => _handleScanResult,
-                overlay: QrScannerOverlayShape(
-                    borderColor: MoabColor.scannerBoarder,
-                    borderRadius: 0,
-                    borderLength: 100,
-                    borderWidth: 2,
-                    cutOutSize: 200,
-                    overlayColor: Theme.of(context).backgroundColor,
-                    cutOutBottomOffset: 120),
+              Container(
+                width: double.infinity,
+                height: 300,
+                alignment: Alignment.center,
+                child: SizedBox(
+                  width: 200,
+                  height: 200,
+                  child: CustomQRView(
+                    onResult: (Barcode code) => _handleScanResult,
+                  ),
+                ),
               ),
               Padding(
                 padding: const EdgeInsets.fromLTRB(0, 200, 0, 0), // TODO
@@ -66,7 +66,7 @@ class ParentScanQRCodeView extends StatelessWidget {
   }
 
   _handleScanResult(Barcode code) {
-    print('Scanned code: ${code.code ?? ''}');
+    print('Scanned code: ${code.rawValue ?? ''}');
     onNext();
   }
 }
