c6aa6db02f184d22fd369687a35400aa9b163647
Austin Chang <austin.chang@belkin.com>
2022-07-06 16:31:29 +0800

    Fix AppLocalizations hook

--- lib/localization/localization_hook.dart
+++ lib/localization/localization_hook.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 AppLocalizations getAppLocalizations(BuildContext context) {
-  return getAppLocalizations(context);
+  return AppLocalizations.of(context)!;
 }
