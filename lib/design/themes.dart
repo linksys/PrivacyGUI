bb7c9f0545238c34fcdf07e9874237119e59575e
Austin Chang <austin.chang@belkin.com>
2022-08-05 11:15:24 +0800

    Update icon Add fonts

--- lib/design/themes.dart
+++ lib/design/themes.dart
@@ -114,31 +114,37 @@ class MoabTheme {
 
   static final _textTheme = Typography().white.copyWith(
         headline1: const TextStyle(
+          fontFamily: 'NeueHaasGrotTextRound',
           fontSize: 22,
           fontStyle: FontStyle.normal,
           fontWeight: FontWeight.w400,
         ),
         headline2: const TextStyle(
+          fontFamily: 'NeueHaasGrotTextRound',
           fontSize: 18,
           fontStyle: FontStyle.normal,
           fontWeight: FontWeight.w400,
         ),
         headline3: const TextStyle(
+          fontFamily: 'NeueHaasGrotTextRound',
           fontSize: 15,
           fontStyle: FontStyle.normal,
           fontWeight: FontWeight.w400,
         ),
         headline4: const TextStyle(
+          fontFamily: 'NeueHaasGrotTextRound',
           fontSize: 13,
           fontStyle: FontStyle.normal,
           fontWeight: FontWeight.w400,
         ),
         button: const TextStyle(
+          fontFamily: 'NeueHaasGrotTextRound',
           fontSize: 16,
           fontStyle: FontStyle.normal,
           fontWeight: FontWeight.w400,
         ),
         bodyText1: const TextStyle(
+          fontFamily: 'NeueHaasGrotTextRound',
           fontSize: 14,
           fontStyle: FontStyle.normal,
           fontWeight: FontWeight.w400,
