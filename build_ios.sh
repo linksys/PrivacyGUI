dca6a1df56740a3f88185f30786bee699b097312
Austin Chang <austin.chang@belkin.com>
2022-08-19 18:56:38 +0800

    update build_ios.sh

--- build_ios.sh
+++ build_ios.sh
@@ -1,7 +1,7 @@
 
 function buildInHouse() {
   version=$1
-  exho "start building in house $version"
+  echo "start building in house $version"
   flutter clean
   flutter build ipa --export-options-plist=ios/Scripts/Moab-EE-InHouse.plist;
   mv "./build/ios/ipa/Moab.ipa" "./build/ios/ipa/moab_app_ee_distribution.ipa"
@@ -22,7 +22,7 @@ function copyInHouseAssets() {
 }
 
 function updateLinks() {
-  exho "Update links"
+  echo "Update links"
   version=$1
   htmlFilePath=./build/ios/ipa/install.html.template
   sed -i '' "s/{version}/$version/g" "$htmlFilePath"
@@ -32,7 +32,7 @@ function updateLinks() {
   sed -i '' "s/Runner/Moab App $version/g" "$manifestPath"
 }
 version=$1
-if buildInHouse "$version"; then
+if ! buildInHouse "$version"; then
   echo InHouse "$version" build failed
   exit 1
 fi
