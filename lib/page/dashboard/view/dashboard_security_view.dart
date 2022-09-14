7205398873f865808a2fc318a5ff4486ba21a3de
Hank Yu <hank.yu@belkin.com>
2022-09-14 18:58:33 +0800

    [Security]
    Add more data models and control logic on the DashboardSecurityView

--- lib/page/dashboard/view/dashboard_security_view.dart
+++ lib/page/dashboard/view/dashboard_security_view.dart
@@ -1,11 +1,21 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:linksys_moab/bloc/profiles/cubit.dart';
 import 'package:linksys_moab/design/colors.dart';
 import 'package:linksys_moab/page/components/shortcuts/sized_box.dart';
 import 'package:linksys_moab/page/components/base_components/base_components.dart';
+
+
+enum SubscriptionStatus {
+  active(displayTitle: 'Active'),
+  activeTrial(displayTitle: 'Active (trial)'),
+  activeTurnedOff(displayTitle: 'Active (Turned Off)'),
+  expired(displayTitle: 'Expired');
+
+  const SubscriptionStatus({required this.displayTitle});
+
+  final String displayTitle;
+}
 
 class DashboardSecurityView extends StatefulWidget {
   const DashboardSecurityView({Key? key}) : super(key: key);
@@ -15,6 +25,23 @@ class DashboardSecurityView extends StatefulWidget {
 }
 
 class _DashboardSecurityViewState extends State<DashboardSecurityView> {
+  //TODO: Remove dummy data
+  var subscriptionPrompt = '30 days left on Linksys Secure trial';
+  // var subscriptionPrompt = 'Your Linksys Secure trial expired';
+  // var subscriptionPrompt = '30% off Linksys Secure subscription';
+  var subscriptionStatus = SubscriptionStatus.active;
+  var numOfInspection = 329;
+  Map<String, int> blockedThreats = {
+    'Virus': 3,
+    'Ransomware & Malware': 6,
+    'Botnet': 11,
+    'Malicious websites': 4,
+  };
+  var hasFilterCreated = true;
+  var numOfIncidents = 21;
+  var evaluatedRange = 'This week';
+  var latestUpdateDate = 'Aug 15, 2022';
+
   @override
   Widget build(BuildContext context) {
     return BasePageView.noNavigationBar(
@@ -27,6 +54,7 @@ class _DashboardSecurityViewState extends State<DashboardSecurityView> {
           _cyberthreatsTile(),
           _divider(),
           _contentFilterTile(),
+          _updateInfo(),
         ],
       ),
     );
@@ -45,8 +73,19 @@ class _DashboardSecurityViewState extends State<DashboardSecurityView> {
           textAlign: TextAlign.start,
         ),
         const SizedBox(
-          height: 32,
+          height: 10,
         ),
+        Container(
+          alignment: Alignment.centerLeft,
+          height: 52,
+          color: MoabColor.dashboardBottomBackground,
+          child: Text(
+            subscriptionPrompt,
+            style: Theme.of(context).textTheme.headline3?.copyWith(
+              fontWeight: FontWeight.w500
+            ),
+          ),
+        )
       ],
     );
   }
@@ -55,11 +94,14 @@ class _DashboardSecurityViewState extends State<DashboardSecurityView> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
+        const SizedBox(
+          height: 45,
+        ),
         Wrap(
           crossAxisAlignment: WrapCrossAlignment.center,
           children: [
             Text(
-              'Linksys Security',
+              'Linksys Secure',
               style: Theme.of(context)
                   .textTheme
                   .headline2
@@ -68,13 +110,13 @@ class _DashboardSecurityViewState extends State<DashboardSecurityView> {
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 8.0),
               child: Image.asset(
-                'assets/images/icon_checked_circle.png',
+                'assets/images/icon_checked_circle.png',//TODO: Remove hard code
                 width: 16,
                 height: 16,
               ),
             ),
             Text(
-              'Active',
+              subscriptionStatus.displayTitle,
               style: Theme.of(context)
                   .textTheme
                   .headline2
@@ -86,10 +128,10 @@ class _DashboardSecurityViewState extends State<DashboardSecurityView> {
           height: 8,
         ),
         Text(
-          'This week',
+          evaluatedRange,  //TODO: Remove hard code
           style: Theme.of(context)
               .textTheme
-              .headline2
+              .headline3
               ?.copyWith(fontWeight: FontWeight.w400),
         ),
       ],
@@ -120,42 +162,61 @@ class _DashboardSecurityViewState extends State<DashboardSecurityView> {
             onPressed: () {},
           ),
         ),
-        SizedBox(
-          height: tileHeight,
-          child: GridView.count(
-            shrinkWrap: true,
-            crossAxisCount: 2,
-            childAspectRatio: 1.75,
-            mainAxisSpacing: 14,
-            crossAxisSpacing: 14,
-            physics: const NeverScrollableScrollPhysics(),
-            children: const [
-              InfoBlockWidget(
-                count: 3,
-                text: 'Virus',
-              ),
-              InfoBlockWidget(
-                count: 6,
-                text: 'Ransomware & Malware',
-              ),
-              InfoBlockWidget(
-                count: 11,
-                text: 'Botnet',
-              ),
-              InfoBlockWidget(
-                count: 4,
-                text: 'Malicious websites',
+        Visibility(
+          visible: blockedThreats.isNotEmpty,
+          replacement: Text(
+            'No one has tried any funny business yet, but we are monitoring 24/7',
+            style: Theme.of(context).textTheme.headline3?.copyWith(
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          child: SizedBox(
+            height: tileHeight,
+            child: GridView.count(
+              shrinkWrap: true,
+              crossAxisCount: 2,
+              childAspectRatio: 1.75,
+              mainAxisSpacing: 14,
+              crossAxisSpacing: 14,
+              physics: const NeverScrollableScrollPhysics(),
+              children: [  //TODO: Remove hard code
+                InfoBlockWidget(
+                  count: 3,
+                  text: 'Virus',
+                  isEnabled: subscriptionStatus != SubscriptionStatus.activeTurnedOff,
+                ),
+                InfoBlockWidget(
+                  count: 6,
+                  text: 'Ransomware & Malware',
+                  isEnabled: subscriptionStatus != SubscriptionStatus.activeTurnedOff,
+                ),
+                InfoBlockWidget(
+                  count: 11,
+                  text: 'Botnet',
+                  isEnabled: subscriptionStatus != SubscriptionStatus.activeTurnedOff,
+                ),
+                InfoBlockWidget(
+                  count: 4,
+                  text: 'Malicious websites',
+                  isEnabled: subscriptionStatus != SubscriptionStatus.activeTurnedOff,
+                ),
+              ],
+            ),
+          ),
+        ),
+        Offstage(
+          offstage: !(numOfInspection > 0),
+          child: Column(
+            children: [
+              box8(),
+              Text(
+                '$numOfInspection total inspections performed',
+                style: Theme.of(context).textTheme.headline3?.copyWith(
+                  fontWeight: FontWeight.w400
+                ),
               ),
             ],
           ),
-        ),
-        box8(),
-        Text(
-          '924 total inspections performed',
-          style: Theme.of(context)
-              .textTheme
-              .headline3
-              ?.copyWith(fontWeight: FontWeight.w400),
         ),
       ],
     );
@@ -168,12 +229,15 @@ class _DashboardSecurityViewState extends State<DashboardSecurityView> {
         TitleWithIcons(
           text: Text(
             'Content filtered',
-            style: Theme.of(context)
-                .textTheme
-                .headline3
-                ?.copyWith(fontWeight: FontWeight.w700),
+            style: Theme.of(context).textTheme.headline3?.copyWith(
+              fontWeight: FontWeight.w700
+            ),
           ),
-          leadingIcon: Image.asset('assets/images/icon_block.png'),
+          leadingIcon: Image.asset(
+            'assets/images/icon_block.png',
+            height: 26,
+            width: 26,
+          ),
           trailingIcon: IconButton(
             icon: Image.asset(
               'assets/images/icon_info.png',
@@ -183,57 +247,83 @@ class _DashboardSecurityViewState extends State<DashboardSecurityView> {
             onPressed: () {},
           ),
         ),
-        const InfoBlockWidget(
-          count: 21,
-          text: 'Blocked content this week',
+        box36(),
+        Visibility(
+          visible: hasFilterCreated,
+          child: Visibility(
+            visible: numOfIncidents > 0,
+            child: _incidentsOnProfiles(),
+            replacement: Text(
+              'No incidents yet',
+              style: Theme.of(context).textTheme.headline3?.copyWith(
+                  fontWeight: FontWeight.w500
+              ),
+            ),
+          ),
+          replacement: _createFilter(),
+        ),
+      ],
+    );
+  }
+
+  Widget _incidentsOnProfiles() {
+    final _mockProfiles = context.read<ProfilesCubit>().state.profileList;
+    final double listHeight = _mockProfiles.length * 80;
+    return Column(
+      children: [
+        InfoBlockWidget(
+          count: numOfIncidents,
+          text: 'Blocked content $evaluatedRange',
+          isEnabled: subscriptionStatus != SubscriptionStatus.activeTurnedOff,
           isVertical: false,
           height: 60,
         ),
-        box4(),
-        _buildContentFilteredProfile(),
-        Text(
-          'Fortinet threat database',
-          style: Theme.of(context).textTheme.headline4?.copyWith(
-            fontWeight: FontWeight.w700
-          ),
-        ),
-        Text(
-          'Last updated on Aug 15, 2022',
-          style: Theme.of(context).textTheme.headline4?.copyWith(
-              fontWeight: FontWeight.w500
+        SizedBox(
+          height: listHeight,
+          child: ListView.builder(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: _mockProfiles.length,
+            itemBuilder: (context, index) => Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: ListTile(
+                shape: const BeveledRectangleBorder(
+                  side: BorderSide(color: Colors.black, width: 1),
+                ),
+                leading: Image.asset(
+                  _mockProfiles[index].icon,
+                  width: 32,
+                  height: 32,
+                ),
+                title: Text(
+                  _mockProfiles[index].name,
+                  style: Theme.of(context).textTheme.bodyText1?.copyWith(fontWeight: FontWeight.w700),
+                ),
+              ),
+            ),
           ),
         ),
       ],
     );
   }
 
-  Widget _buildContentFilteredProfile() {
-    final _mockProfiles = context.read<ProfilesCubit>().state.profileList;
-    final double listHeight = _mockProfiles.length * 80;
-    return SizedBox(
-      height: listHeight,
-      child: ListView.builder(
-        shrinkWrap: true,
-        physics: const NeverScrollableScrollPhysics(),
-        itemCount: _mockProfiles.length,
-        itemBuilder: (context, index) => Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8.0),
-          child: ListTile(
-            shape: const BeveledRectangleBorder(
-              side: BorderSide(color: Colors.black, width: 1),
-            ),
-            leading: Image.asset(
-              _mockProfiles[index].icon,
-              width: 32,
-              height: 32,
-            ),
-            title: Text(
-              _mockProfiles[index].name,
-              style: Theme.of(context).textTheme.bodyText1?.copyWith(fontWeight: FontWeight.w700),
-            ),
+  Widget _createFilter() {
+    return Column(
+      children: [
+        Text(
+          'Create healthy digital lifestyle for your family and feel safe with the content on your network. Filter out categories and apps you don’t want.',
+          style: Theme.of(context).textTheme.headline3?.copyWith(
+              fontWeight: FontWeight.w500
           ),
         ),
-      ),
+        box24(),
+        PrimaryButton(
+          text: 'Create filter',
+          onPress: () {
+            //TODO: Go to next page
+          },
+        ),
+      ],
     );
   }
 
@@ -244,6 +334,27 @@ class _DashboardSecurityViewState extends State<DashboardSecurityView> {
         height: 1,
         color: MoabColor.black,
       ),
+    );
+  }
+
+  Widget _updateInfo() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        box48(),
+        Text(
+          'Fortinet threat database',
+          style: Theme.of(context).textTheme.headline4?.copyWith(
+              fontWeight: FontWeight.w700
+          ),
+        ),
+        Text(
+          'Last updated on $latestUpdateDate',
+          style: Theme.of(context).textTheme.headline4?.copyWith(
+              fontWeight: FontWeight.w500
+          ),
+        ),
+      ],
     );
   }
 }
@@ -288,12 +399,16 @@ class InfoBlockWidget extends StatelessWidget {
     Key? key,
     required this.count,
     required this.text,
+    this.isEnabled = true,
     this.color = MoabColor.dashboardBottomBackground,
+    this.disabledColor = MoabColor.dashboardDisabled,
     this.isVertical = true,
     this.height = 0,
   }) : super(key: key);
 
+  final bool isEnabled;
   final Color color;
+  final Color disabledColor;
   final int count;
   final String text;
   final bool isVertical;
@@ -302,7 +417,7 @@ class InfoBlockWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: color,
+      color: isEnabled ? color : disabledColor,
       height: height,
       alignment: Alignment.centerLeft,
       padding: const EdgeInsets.all(12),
