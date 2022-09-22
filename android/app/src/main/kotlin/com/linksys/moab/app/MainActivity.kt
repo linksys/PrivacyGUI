d22ede0e5543a0572291386cff17d044d2ecf443
Austin Chang <austin.chang@belkin.com>
2022-09-22 12:17:47 +0800

    Remove dummy code on MainActivity.kt

--- android/app/src/main/kotlin/com/linksys/moab/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/linksys/moab/app/MainActivity.kt
@@ -1,35 +1,7 @@
 package com.linksys.moab.app
 
-import android.Manifest
-import android.annotation.SuppressLint
-import android.app.Activity
-import android.content.*
-import android.content.pm.PackageManager
-import android.net.wifi.WifiConfiguration
-import android.net.wifi.WifiManager
-import android.net.wifi.WifiNetworkSuggestion
-import android.os.Build
-import android.util.Log
-import androidx.annotation.NonNull
-import androidx.annotation.RequiresApi
-import androidx.core.content.ContextCompat
-import com.google.android.gms.auth.api.phone.SmsRetriever
-import com.google.android.gms.common.api.CommonStatusCodes
-import com.google.android.gms.common.api.Status
-import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.android.FlutterFragmentActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugin.common.EventChannel
-import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity : FlutterFragmentActivity() {
 
-    private val OTP_CHANNEL = "com.linksys.native.channel.otp"
-    private var otpResult: MethodChannel.Result? = null
-
-    @RequiresApi(Build.VERSION_CODES.Q)
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-    }
 }
