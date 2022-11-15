b05fe08fcf41237600c26c37c641ac0690033019
Austin Chang <austin.chang@belkin.com>
2022-11-15 16:33:19 +0800

    [Content filter] String converter test case

--- test/test_string_converter.dart
+++ test/test_string_converter.dart
@@ -1,15 +1,23 @@
 
 import 'dart:convert';
+import 'dart:math';
 import 'dart:typed_data';
 
 import 'package:linksys_moab/utils.dart';
 import 'package:test/test.dart';
 
+// TODO test supported languages
 void main() {
-  group('test string conveter', () {
+  group('test string converter', () {
     test('test encoded Chinese', () async {
       const test = '中文';
-      Utils.fullStringEncoded(test);
+      final encoded = Utils.fullStringEncoded(test);
+      expect(Utils.fullStringDecoded(encoded), test);
+    });
+    test('test encoded Tai', () async {
+      const test = 'ทดสอบ';
+      final encoded = Utils.fullStringEncoded(test);
+      expect(Utils.fullStringDecoded(encoded), test);
     });
   });
 }
