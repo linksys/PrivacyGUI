39d3a01828c3320ab816bb04990f560a820f0b19
Peter Jhong <peter.jhong@belkin.com>
2022-11-28 15:30:47 +0800

    [BaseText] Add base text

--- lib/page/components/base_components/base_components.dart
+++ lib/page/components/base_components/base_components.dart
@@ -1,7 +1,7 @@
 export 'base_page_view.dart';
 export 'selectable_item/selectable_item.dart';
 export 'selectable_item/checkbox_selectable_item.dart';
-export 'text/text.dart';
+export 'text/_text.dart';
 export 'progress_bars/progress_bars.dart';
 export 'input_fields/_input_fields.dart';
 export 'button/button.dart';
--- lib/page/components/base_components/text/text.dart
+++
@@ -1,2 +0,0 @@
-export 'description_text.dart';
-export 'title_text.dart';
