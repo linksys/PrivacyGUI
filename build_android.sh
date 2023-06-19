b9b83ffc5cfb6d07a667dab123bbea9f05b380a2
Austin Chang <austin.chang@belkin.com>
2023-06-19 09:07:10 +0800

    Update build_android.sh script

--- build_android.sh
+++ build_android.sh
@@ -2,8 +2,13 @@
 function build() {
   conf=$1
   type=$2
-  echo start building "$conf" "$type" process...
-  if ! flutter build "$type" --"$conf" --obfuscate --split-debug-info=moab/build/app/outputs/temp/ --build-number="${AppBuildNumber}" --dart-define=cloud_env=qa --no-tree-shake-icons; then
+  env=$3
+  hasPicker=true
+  if ["type" = "appbundle" ]; then
+    hasPicker=false
+  fi
+  echo start building "$conf" "$type" "$env" process...
+  if ! flutter build "$type" --"$conf" --obfuscate --split-debug-info=moab/build/app/outputs/temp/ --build-number="${AppBuildNumber}" --dart-define=cloud_env="${env}" --dart-define=enable_env_picker="${hasPicker}" --no-tree-shake-icons; then
       echo build "$conf" failed
       exit 1
   fi
@@ -11,8 +16,8 @@ function build() {
   if [ "$type" = "appbundle" ]; then
     ext="aab"
   fi
-  copyFiles "$ext" "$conf"
-  echo finish build "$conf" "$type".
+  copyFiles "$ext" "$conf" "$env"
+  echo finish build "$conf" "$type" "$env".
 }
 
 function copyFiles() {
@@ -21,27 +26,36 @@ function copyFiles() {
 
   type=$1
   conf=$2
+  env=$3
+  if ["$env" = "prod"]; then
+    env=""
+  else
+    env="-$env"
+  fi
   path=apk
   if [ "$type" = "aab" ]; then
     echo "process app bundle..."
     path="bundle"
     confFilePath=$(ls ./build/app/outputs/"$path"/"$conf"/*."$type")
     confFilename=$(basename "$confFilePath" ."$type")
-    cp "$confFilePath" "$targetFlutterApkPath"/"$confFilename-$BranchName-$AppBuildNumber"."$type"
+    cp "$confFilePath" "$targetFlutterApkPath"/"$confFilename-$BranchName$env-$AppBuildNumber"."$type"
   else
     echo "process apk..."
     confFilePath=$(ls ./build/app/outputs/"$path"/"$conf"/*."$type")
     confFilename=$(basename "$confFilePath" ."$type")
-    cp "$confFilePath" "$targetFlutterApkPath"/"$confFilename-$BranchName-$AppBuildNumber"."$type"
+    cp "$confFilePath" "$targetFlutterApkPath"/"$confFilename-$BranchName$env-$AppBuildNumber"."$type"
   fi
 }
 BranchName=$1
 AppBuildNumber=$2
+
 flutter --version
 flutter pub get
 flutter pub deps
 flutter clean
 flutter pub cache repairbuild debug apk
-build release apk
-build debug apk
-build release appbundle
+build debug apk qa
+build release apk qa
+build debug apk prod
+build release apk prod
+build release appbundle prod
