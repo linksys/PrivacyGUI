c0405c358ceb11b4df8e83e35504b4f5f47f8ad8
Austin Chang <austin.chang@belkin.com>
2023-09-22 16:12:21 +0800

    Fix generate qr code

--- ios/Scripts/qr_code_generator.dart
+++ ios/Scripts/qr_code_generator.dart
@@ -1,17 +1,16 @@
 import 'dart:io';
-import 'dart:typed_data';
 
 import 'package:image/image.dart';
 import 'package:qr/qr.dart';
 
-void main(List<String> args) {
+void main(List<String> args) async {
   if (args.isEmpty) {
     exit(0);
   }
   final String data = args.first;
   final qrCode = QrCode(4, QrErrorCorrectLevel.L)..addData(data);
   final qr = QrImage(qrCode);
-  final image = _drawQRCodeDefault(qr);
+  final image = await _drawQRCodeDefault(qr);
 
   File file = File('ios/Scripts/InHouse/qr_code.png');
   if (!file.existsSync()) {
@@ -20,8 +19,8 @@ void main(List<String> args) {
   file.writeAsBytesSync(encodePng(image));
 }
 
-Image _drawQRCodeDefault(QrImage qr) {
-  int size = 240;
+Future<Image> _drawQRCodeDefault(QrImage qr) async {
+  int size = 360;
   int blockSize = (size / qr.moduleCount).floor() + 2;
   int imageSize = (blockSize * qr.moduleCount) + (blockSize * 2);
 
@@ -29,23 +28,34 @@ Image _drawQRCodeDefault(QrImage qr) {
     width: imageSize,
     height: imageSize,
   );
-  fill(img, color: ColorInt16.rgb(255, 255, 255));
+  img.clear(ColorFloat16.rgb(255, 255, 255));
 
+  String str = '';
   for (var x = 0; x < qr.moduleCount; x++) {
     for (var y = 0; y < qr.moduleCount; y++) {
-      if (qr.isDark(x, y) == false) continue;
+      if (qr.isDark(x, y) == false) {
+        str += ' ';
+        continue;
+      }
+      str += '*';
       int xx = (x * blockSize) + blockSize;
       int yy = (y * blockSize) + blockSize;
-      fillRect(
-        img,
-        x1: xx,
-        y1: yy,
-        x2: xx + blockSize,
-        y2: yy + blockSize,
-        color: ColorInt16.rgb(0, 0, 0),
-      );
+
+      for (var d = yy; d <= yy + blockSize; d++) {
+        drawLine(img,
+            x1: xx,
+            y1: d,
+            x2: xx + blockSize,
+            y2: d,
+            color: ColorUint8.rgb(0, 75, 193));
+      }
     }
+    str += '\n';
   }
+  var icon = await decodePngFile('ios/Scripts/InHouse/ic_launcher.png');
+  icon = copyResize(icon!, width: 64, height: 64);
+  compositeImage(img, icon, center: true);
+  print(str);
 
   return copyResize(img, width: size, height: size);
 }
