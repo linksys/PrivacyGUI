5e2e38d5da4f819091a8a4f0599dd2d79ca84272
Austin Chang <austin.chang@belkin.com>
2024-02-07 10:40:16 +0800

    Add test cases

--- test/validator_rules/validators_test.dart
+++ test/validator_rules/validators_test.dart
@@ -44,4 +44,99 @@ void main() {
       expect(detail.length, 3);
     });
   });
+
+  group('IpAddressLocalTestSubnetMaskValidator', () {
+    test('router ip 192.168.1.1 and subnet mask 255.255.255.0', () async {
+      final validator =
+          IpAddressLocalTestSubnetMaskValidator('192.168.1.1', '255.255.255.0');
+      for (int i = 1; i < 256; i++) {
+        expect(validator.validate('192.168.1.$i'), true);
+      }
+      for (int i = 1; i < 256; i++) {
+        expect(validator.validate('192.168.2.$i'), false);
+      }
+
+      expect(validator.validate(''), false);
+      expect(validator.validate('abc'), false);
+      expect(validator.validate('192.1'), false);
+      expect(validator.validate('192.166.1.1'), false);
+      expect(validator.validate('192.168.21.1'), false);
+      expect(validator.validate('12.168.21.1'), false);
+      expect(validator.validate('192.18.21.1'), false);
+    });
+
+    test('router ip 192.168.1.1 and subnet mask 255.255.0.0', () async {
+      final validator =
+          IpAddressLocalTestSubnetMaskValidator('192.168.1.1', '255.255.0.0');
+      for (int i = 1; i < 256; i++) {
+        expect(validator.validate('192.168.1.$i'), true);
+      }
+      for (int i = 1; i < 256; i++) {
+        expect(validator.validate('192.168.2.$i'), true);
+      }
+      for (int i = 1; i < 256; i++) {
+        for (int j = 1; j < 256; j++) {
+          expect(validator.validate('192.168.$i.$j'), true);
+        }
+      }
+
+      expect(validator.validate(''), false);
+      expect(validator.validate('abc'), false);
+      expect(validator.validate('192.1'), false);
+      expect(validator.validate('192.166.1.1'), false);
+      expect(validator.validate('192.168.21.1'), true);
+      expect(validator.validate('12.168.21.1'), false);
+      expect(validator.validate('192.18.21.1'), false);
+    });
+  });
+
+  group('IpAddressLocalValidator', () {
+    test('router ip 192.168.1.1 and subnet mask 255.255.255.0', () async {
+      final validator = IpAddressLocalValidator('192.168.1.1', '255.255.255.0');
+
+      expect(validator.validate('192.168.1.1'), false);
+
+      for (int i = 2; i < 256; i++) {
+        expect(validator.validate('192.168.1.$i'), true);
+      }
+      for (int i = 1; i < 256; i++) {
+        expect(validator.validate('192.168.2.$i'), false);
+      }
+
+      expect(validator.validate(''), false);
+      expect(validator.validate('abc'), false);
+      expect(validator.validate('192.1'), false);
+      expect(validator.validate('192.166.1.1'), false);
+      expect(validator.validate('192.168.21.1'), false);
+      expect(validator.validate('12.168.21.1'), false);
+      expect(validator.validate('192.18.21.1'), false);
+    });
+
+    test('router ip 192.168.1.1 and subnet mask 255.255.0.0', () async {
+      final validator = IpAddressLocalValidator('192.168.1.1', '255.255.0.0');
+
+      expect(validator.validate('192.168.1.1'), false);
+
+      for (int i = 2; i < 256; i++) {
+        expect(validator.validate('192.168.1.$i'), true);
+      }
+      for (int i = 1; i < 256; i++) {
+        expect(validator.validate('192.168.2.$i'), true);
+      }
+      for (int i = 1; i < 256; i++) {
+        for (int j = 1; j < 256; j++) {
+          expect(validator.validate('192.168.$i.$j'),
+              i == 1 && j == 1 ? false : true);
+        }
+      }
+
+      expect(validator.validate(''), false);
+      expect(validator.validate('abc'), false);
+      expect(validator.validate('192.1'), false);
+      expect(validator.validate('192.166.1.1'), false);
+      expect(validator.validate('192.168.21.1'), true);
+      expect(validator.validate('12.168.21.1'), false);
+      expect(validator.validate('192.18.21.1'), false);
+    });
+  });
 }
