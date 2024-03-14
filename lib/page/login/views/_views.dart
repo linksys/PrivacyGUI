f6218e5232f53848a5d65873cc342a50be31b4ad
Hank Yu <hank.yu@belkin.com>
2024-03-14 15:45:24 +0800

    [Local Login]
    Rename and remove files

--- lib/page/login/views/_views.dart
+++ lib/page/login/views/_views.dart
@@ -1,4 +1,3 @@
-export 'local_login_router_password_view.dart';
 export 'local_router_recovery_view.dart';
 export 'forgot_email_view.dart';
 export 'cloud_forgot_password_view.dart';
@@ -6,5 +5,5 @@ export 'cloud_login_account_view.dart';
 export 'cloud_login_traditional_password_view.dart';
 export 'cloud_reset_password_view.dart';
 export 'cloud_ready_for_login_view.dart';
-export 'login_view.dart';
+export 'login_local_view.dart';
 export 'login_cloud_view.dart';
