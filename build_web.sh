61fa6c4d3613a32f57f0f253bfa6e4916a7b184f
Austin Chang <austin.chang@belkin.com>
2024-04-02 10:48:55 +0800

    Add href parameter for build_web.sh

--- build_web.sh
+++ build_web.sh
@@ -1,16 +1,14 @@
 function buildWebApp() {
   echo "start building web app #${buildNumber}-${force}"
-  baseHref="/app/"
-  if [ "$force" = "local" ]; then
-    baseHref="/ui/local/dynamic/"
-  fi
-  echo "base href is ${baseHref}"
-  flutter build web --target=lib/main.dart --base-href="${baseHref}" --build-number="${buildNumber}" --dart-define=force="${force}" --web-renderer html --no-tree-shake-icons
+  echo "base href is ${href}"
+  
+  flutter build web --target=lib/main.dart --base-href="${href}" --build-number="${buildNumber}" --dart-define=force="${force}" --web-renderer html --no-tree-shake-icons
   cp -r "./build/web" "./artifacts/webApp/"
 }
 
 buildNumber=$1
 force=$2
+href=$3
 
 if ! buildWebApp "$buildNumber"; then
     echo Web App "$buildNumber" build failed
