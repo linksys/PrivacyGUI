bda55f91dc35b40d4c3701ea9f5a7bfb2beda678
Austin Chang <austin.chang@belkin.com>
2024-04-09 10:21:00 +0800

    Update script

--- build_web.sh
+++ build_web.sh
@@ -2,7 +2,7 @@ function buildWebApp() {
   echo "start building web app #${buildNumber}-${force}"
   echo "base href is ${href}"
 
-  flutter build web --target=lib/main.dart --base-href="/${href}/" --build-number="${buildNumber}" --dart-define=force="${force}" --web-renderer html --no-tree-shake-icons
+  flutter build web --target=lib/main.dart --base-href="/${href}" --build-number="${buildNumber}" --dart-define=force="${force}" --web-renderer html --no-tree-shake-icons
   cp -r "./build/web" "./artifacts/webApp/"
 }
 
