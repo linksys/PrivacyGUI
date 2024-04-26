41ecc55398353df4c72e0bf4785c4f93db495f09
Austin Chang <austin.chang@belkin.com>
2024-04-26 11:45:33 +0800

    fix ping cloud without default header

--- lib/core/cloud/linksys_requests/ping_service.dart
+++ lib/core/cloud/linksys_requests/ping_service.dart
@@ -7,11 +7,11 @@ import 'package:linksys_app/core/http/linksys_http_client.dart';
 extension PingService on LinksysHttpClient {
   Future<Response> testPingPng() {
     final endpoint = combineUrl(kTestPingPng);
-    final header = {
+    final header = defaultHeader..addAll({
       HttpHeaders.contentTypeHeader: 'image/png',
       HttpHeaders.expiresHeader: 'Fri, 10 Oct 2015 14:19:41 GMT',
       HttpHeaders.cacheControlHeader: 'no-cache',
-    };
+    });
     return this.get(Uri.parse(endpoint), headers: header);
   }
 }
