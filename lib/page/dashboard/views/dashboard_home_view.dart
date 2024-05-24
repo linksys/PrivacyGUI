892c345ccec3adc4a3a7f8210c59dc9154dac0a8
Austin Chang <austin.chang@belkin.com>
2024-05-24 14:23:57 +0800

    Import strings fix minor issues on dashboard

--- lib/page/dashboard/views/dashboard_home_view.dart
+++ lib/page/dashboard/views/dashboard_home_view.dart
@@ -1,12 +1,7 @@
 import 'package:collection/collection.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:go_router/go_router.dart';
-import 'package:privacy_gui/constants/_constants.dart';
 import 'package:privacy_gui/core/jnap/providers/device_manager_provider.dart';
 import 'package:privacy_gui/core/jnap/providers/device_manager_state.dart';
 import 'package:privacy_gui/core/jnap/providers/firmware_update_provider.dart';
@@ -19,20 +14,18 @@ import 'package:privacy_gui/page/firmware_update/_firmware_update.dart';
 import 'package:privacy_gui/page/topology/_topology.dart';
 import 'package:privacy_gui/page/dashboard/providers/dashboard_home_provider.dart';
 import 'package:privacy_gui/page/dashboard/providers/dashboard_home_state.dart';
-import 'package:privacy_gui/page/dashboard/providers/smart_device_provider.dart';
 import 'package:privacy_gui/route/constants.dart';
 import 'package:privacy_gui/util/extensions.dart';
-import 'package:privacy_gui/util/smart_device_prefs_helper.dart';
 import 'package:privacygui_widgets/hook/icon_hooks.dart';
 import 'package:privacygui_widgets/icons/linksys_icons.dart';
 import 'package:privacygui_widgets/theme/_theme.dart';
 import 'package:privacygui_widgets/theme/const/spacing.dart';
 import 'package:privacygui_widgets/widgets/_widgets.dart';
 import 'package:privacygui_widgets/widgets/card/card.dart';
+import 'package:privacygui_widgets/widgets/card/list_card.dart';
 import 'package:privacygui_widgets/widgets/container/responsive_layout.dart';
 import 'package:privacygui_widgets/widgets/progress_bar/spinner.dart';
 
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:shimmer/shimmer.dart';
 
 class DashboardHomeView extends ConsumerStatefulWidget {
@@ -49,7 +42,7 @@ class _DashboardHomeViewState extends ConsumerState<DashboardHomeView> {
   void initState() {
     super.initState();
     firmware = ref.read(firmwareUpdateProvider.notifier);
-    _pushNotificationCheck();
+    // _pushNotificationCheck();
     _firmwareUpdateCheck();
   }
 
@@ -135,8 +128,10 @@ class _DashboardHomeViewState extends ConsumerState<DashboardHomeView> {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         // _ssid(ssid, isLoading),
-        _title(),
-        const AppGap.regular(),
+        if (isOnline) ...[
+          _title(),
+          const AppGap.regular(),
+        ],
         Stack(
           children: [
             AnimatedOpacity(
@@ -172,18 +167,20 @@ class _DashboardHomeViewState extends ConsumerState<DashboardHomeView> {
             ),
           ],
         ),
-        hasNewFirmware()
-            ? AppTextButton(
-                'New Firmware Availiable',
-                onTap: () {
-                  showAdaptiveDialog(
-                      context: context,
-                      builder: (context) {
-                        return FirmwareUpdateDetailView();
-                      });
-                },
-              )
-            : Center(),
+        if (!isLoading && !isOnline) _troubleshooting(),
+        if (isOnline && hasNewFirmware())
+          AppCard(
+            child: AppTextButton(
+              loc(context).newFirmwareAvailable,
+              onTap: () {
+                showAdaptiveDialog(
+                    context: context,
+                    builder: (context) {
+                      return FirmwareUpdateDetailView();
+                    });
+              },
+            ),
+          ),
       ],
     );
   }
@@ -203,15 +200,16 @@ class _DashboardHomeViewState extends ConsumerState<DashboardHomeView> {
 
   Widget _title() {
     final now = TimeOfDay.now();
+    const morning = TimeOfDay(hour: 6, minute: 0);
     const noon = TimeOfDay(hour: 12, minute: 0);
-    const night = TimeOfDay(hour: 18, minute: 0);
+    const evening = TimeOfDay(hour: 18, minute: 0);
     String text;
-    if (now.compareTo(noon) < 0) {
-      text = 'Good morning!';
-    } else if (now.compareTo(night) < 0) {
-      text = 'Good afternoon!';
+    if (now.compareTo(morning) > 0 && now.compareTo(noon) < 0) {
+      text = loc(context).goodMorning;
+    } else if (now.compareTo(evening) < 0) {
+      text = loc(context).goodAfternoon;
     } else {
-      text = 'Good night';
+      text = loc(context).goodEvening;
     }
     return AppText.headlineSmall(text);
   }
@@ -320,29 +318,12 @@ class _DashboardHomeViewState extends ConsumerState<DashboardHomeView> {
     );
   }
 
-  Widget _speedTestTile(DashboardHomeState state, bool isLoading) {
-    return GestureDetector(
-        onTap: () => context.goNamed(RouteNamed.dashboardSpeedTest),
-        child: SizedBox(
-          width: double.infinity,
-          height: 160,
-          child: Card(
-            elevation: 10,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                  horizontal: Spacing.regular, vertical: Spacing.regular),
-              child: isLoading
-                  ? Shimmer(
-                      gradient: _shimmerGradient,
-                      child: _speedResult(state),
-                    )
-                  : _speedResult(state),
-            ),
-          ),
-        ));
+  Widget _troubleshooting() {
+    return AppListCard(
+      title: AppText.labelLarge(loc(context).troubleshoot),
+      trailing: const Icon(LinksysIcons.chevronRight),
+      onTap: () {},
+    );
   }
 
   get _shimmerGradient => LinearGradient(
@@ -361,104 +342,9 @@ class _DashboardHomeViewState extends ConsumerState<DashboardHomeView> {
         tileMode: TileMode.clamp,
       );
 
-  Widget _speedResult(DashboardHomeState state) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        AppText.titleLarge('Speed'),
-        Expanded(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        const AppGap.semiSmall(),
-                        AppText.titleLarge(state.uploadResult.value),
-                      ],
-                    ),
-                    Text('${state.uploadResult.unit}ps'),
-                  ],
-                ),
-              ),
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        const AppGap.semiSmall(),
-                        AppText.titleLarge(state.downloadResult.value),
-                      ],
-                    ),
-                    Text('${state.downloadResult.unit}ps'),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ],
-    );
-  }
-
   void _firmwareUpdateCheck() {
     Future.doWhile(() => !mounted).then((_) {
       firmware.checkFirmwareUpdateStatus();
-    });
-  }
-
-  void _pushNotificationCheck() {
-    if (kIsWeb) {
-      return;
-    }
-    if (!mounted) {
-      return;
-    }
-    if (GoRouter.of(context).routerDelegate.currentConfiguration.fullPath !=
-        RoutePath.dashboardHome) {
-      return;
-    }
-    SharedPreferences.getInstance().then((prefs) {
-      final isPushPromptShown = prefs.getBool(
-              SmartDevicesPrefsHelper.getNidKey(prefs, key: pShowPushPrompt)) ??
-          false;
-      if (!isPushPromptShown) {
-        prefs.setBool(
-            SmartDevicesPrefsHelper.getNidKey(prefs, key: pShowPushPrompt),
-            true);
-        showAdaptiveDialog(
-          context: context,
-          builder: (context) => AlertDialog(
-            title: AppText.bodyLarge('Push Notification'),
-            content: AppText.bodyLarge(
-                'Do you want to receive Linksys push notifications?'),
-            actions: [
-              AppTextButton(
-                'Yes',
-                onTap: () {
-                  final deviceToken = prefs.getString(pDeviceToken);
-                  if (deviceToken != null) {
-                    ref
-                        .read(smartDeviceProvider.notifier)
-                        .registerSmartDevice(deviceToken);
-                  } else {}
-                  context.pop();
-                },
-              ),
-              AppTextButton('No', onTap: () {
-                context.pop();
-              })
-            ],
-          ),
-        );
-      }
     });
   }
 
@@ -468,4 +354,124 @@ class _DashboardHomeViewState extends ConsumerState<DashboardHomeView> {
     return nodesStatus?.any((element) => element.availableUpdate != null) ??
         false;
   }
+
+  // Widget _speedTestTile(DashboardHomeState state, bool isLoading) {
+  //   return GestureDetector(
+  //       onTap: () => context.goNamed(RouteNamed.dashboardSpeedTest),
+  //       child: SizedBox(
+  //         width: double.infinity,
+  //         height: 160,
+  //         child: Card(
+  //           elevation: 10,
+  //           shape: RoundedRectangleBorder(
+  //             borderRadius: BorderRadius.circular(10.0),
+  //           ),
+  //           child: Padding(
+  //             padding: const EdgeInsets.symmetric(
+  //                 horizontal: Spacing.regular, vertical: Spacing.regular),
+  //             child: isLoading
+  //                 ? Shimmer(
+  //                     gradient: _shimmerGradient,
+  //                     child: _speedResult(state),
+  //                   )
+  //                 : _speedResult(state),
+  //           ),
+  //         ),
+  //       ));
+  // }
+
+  // Widget _speedResult(DashboardHomeState state) {
+  //   return Column(
+  //     mainAxisAlignment: MainAxisAlignment.start,
+  //     crossAxisAlignment: CrossAxisAlignment.start,
+  //     children: [
+  //       AppText.titleLarge('Speed'),
+  //       Expanded(
+  //         child: Row(
+  //           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  //           children: [
+  //             Expanded(
+  //               child: Column(
+  //                 mainAxisAlignment: MainAxisAlignment.center,
+  //                 crossAxisAlignment: CrossAxisAlignment.start,
+  //                 children: [
+  //                   Row(
+  //                     children: [
+  //                       const AppGap.semiSmall(),
+  //                       AppText.titleLarge(state.uploadResult.value),
+  //                     ],
+  //                   ),
+  //                   Text('${state.uploadResult.unit}ps'),
+  //                 ],
+  //               ),
+  //             ),
+  //             Expanded(
+  //               child: Column(
+  //                 mainAxisAlignment: MainAxisAlignment.center,
+  //                 crossAxisAlignment: CrossAxisAlignment.start,
+  //                 children: [
+  //                   Row(
+  //                     children: [
+  //                       const AppGap.semiSmall(),
+  //                       AppText.titleLarge(state.downloadResult.value),
+  //                     ],
+  //                   ),
+  //                   Text('${state.downloadResult.unit}ps'),
+  //                 ],
+  //               ),
+  //             ),
+  //           ],
+  //         ),
+  //       ),
+  //     ],
+  //   );
+  // }
+
+  // void _pushNotificationCheck() {
+  //   if (kIsWeb) {
+  //     return;
+  //   }
+  //   if (!mounted) {
+  //     return;
+  //   }
+  //   if (GoRouter.of(context).routerDelegate.currentConfiguration.fullPath !=
+  //       RoutePath.dashboardHome) {
+  //     return;
+  //   }
+  //   SharedPreferences.getInstance().then((prefs) {
+  //     final isPushPromptShown = prefs.getBool(
+  //             SmartDevicesPrefsHelper.getNidKey(prefs, key: pShowPushPrompt)) ??
+  //         false;
+  //     if (!isPushPromptShown) {
+  //       prefs.setBool(
+  //           SmartDevicesPrefsHelper.getNidKey(prefs, key: pShowPushPrompt),
+  //           true);
+  //       showAdaptiveDialog(
+  //         context: context,
+  //         builder: (context) => AlertDialog(
+  //           title: AppText.bodyLarge('Push Notification'),
+  //           content: AppText.bodyLarge(
+  //               'Do you want to receive Linksys push notifications?'),
+  //           actions: [
+  //             AppTextButton(
+  //               'Yes',
+  //               onTap: () {
+  //                 final deviceToken = prefs.getString(pDeviceToken);
+  //                 if (deviceToken != null) {
+  //                   ref
+  //                       .read(smartDeviceProvider.notifier)
+  //                       .registerSmartDevice(deviceToken);
+  //                 } else {}
+  //                 context.pop();
+  //               },
+  //             ),
+  //             AppTextButton('No', onTap: () {
+  //               context.pop();
+  //             })
+  //           ],
+  //         ),
+  //       );
+  //     }
+  //   });
+  // }
 }
