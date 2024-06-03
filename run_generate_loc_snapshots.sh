660581989b9cd591ab008a38b92a10bc0aa5b9f5
Austin Chang <austin.chang@belkin.com>
2024-06-03 16:21:11 +0800

    - update run_generage_loc_snapshots.sh - add pnp_admin_view_test

--- run_generate_loc_snapshots.sh
+++ run_generate_loc_snapshots.sh
@@ -1,9 +1,10 @@
-while getopts l:s:f: flag
+while getopts l:s:f:c: flag
 do
     case "${flag}" in
         l) locales=${OPTARG};;
         s) screens=${OPTARG};;
         f) file=${OPTARG};;
+        c) copy=true;;
     esac
 done
 if [ -z "$locales" ]; then
@@ -28,6 +29,10 @@ echo all screenshots to "snapshots" folder
 mkdir ./snapshots/
 for dir in $(find ./ -iname 'goldens' -type d); do
   echo $dir
-  find "$dir" -type f -exec mv {} ./snapshots/ \;
+  if [ "$copy" = true ]; then
+    find "$dir" -type f -exec cp {} ./snapshots/ \;
+  else
+    find "$dir" -type f -exec mv {} ./snapshots/ \;
+  fi
 done
 dart run test_scripts/grep_loc_fils.dart
--- test/page/pnp/localizations/pnp_admin_view_test.dart
+++ test/page/pnp/localizations/pnp_admin_view_test.dart
@@ -1,11 +1,59 @@
+import 'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:privacy_gui/core/jnap/models/device_info.dart';
+import 'package:privacy_gui/core/jnap/router_repository.dart';
+import 'package:privacy_gui/page/pnp/data/pnp_exception.dart';
 import 'package:privacy_gui/page/pnp/data/pnp_provider.dart';
+import 'package:privacy_gui/page/pnp/data/pnp_state.dart';
 import 'package:privacy_gui/page/pnp/pnp_admin_view.dart';
 
 import '../../../common/test_responsive_widget.dart';
 import '../../../common/testable_router.dart';
+import '../../../test_data/device_info_test_data.dart';
 
+@GenerateNiceMocks([MockSpec<PnpNotifier>(), MockSpec<RouterRepository>()])
 void main() {
-  testLocalizations('pnp admin view ', (tester, locale) async {
+  late MockPnpNotifier mockPnpNotifier;
+
+  setUp(() {
+    mockPnpNotifier = MockPnpNotifier();
+    // when(mockPnpNotifier.build()).thenReturn(PnpState(
+    //     deviceInfo:
+    //         NodeDeviceInfo.fromJson(jsonDecode(testDeviceInfo)['output'])));
+    // when(mockPnpNotifier.checkAdminPassword(null))
+    //     .thenThrow(ExceptionInvalidAdminPassword());
+    // when(mockPnpNotifier.fetchDeviceInfo()).thenAnswer((_) async => {});
+    // when(mockPnpNotifier.checkInternetConnection()).thenAnswer((_) async => {});
+    // when(mockPnpNotifier.checkRouterConfigured()).thenAnswer((_) async => {});
+    // when(mockPnpNotifier.save()).thenAnswer((_) async => {});
+  });
+  testLocalizations('pnp admin view - input router password ',
+      (tester, locale) async {
+    await tester.pumpWidget(
+      testableSingleRoute(
+        child: const PnpAdminView(),
+        locale: locale,
+        overrides: [pnpProvider.overrideWith(() => mockPnpNotifier)],
+      ),
+    );
+    await tester.pump(const Duration(seconds: 6));
+  });
+  testLocalizations('pnp admin view - checking internet ',
+      (tester, locale) async {
+    await tester.pumpWidget(
+      testableSingleRoute(
+        child: const PnpAdminView(),
+        locale: locale,
+        overrides: [pnpProvider.overrideWith(() => TestMockPnpNotifier())],
+      ),
+    );
+  });
+
+  testLocalizations('pnp admin view - internet connected',
+      (tester, locale) async {
     await tester.pumpWidget(
       testableSingleRoute(
         child: const PnpAdminView(),
@@ -14,21 +62,21 @@ void main() {
       ),
     );
     await tester.pumpAndSettle();
-    await tester.pumpAndSettle();
+    await tester.pump(Duration(seconds: 1));
   });
 }
 
 class TestMockPnpNotifier extends BasePnpNotifier {
   @override
-  Future checkAdminPassword(String? password) async {}
-
-  @override
-  Future checkInternetConnection() async {
+  Future checkAdminPassword(String? password) async {
+    throw ExceptionInvalidAdminPassword();
   }
 
   @override
-  Future fetchDeviceInfo() async {
-  }
+  Future checkInternetConnection() async {}
+
+  @override
+  Future fetchDeviceInfo() async {}
 
   @override
   Future<bool> pnpCheck() async {
@@ -66,6 +114,7 @@ class TestMockPnpNotifier extends BasePnpNotifier {
 
   @override
   Future checkRouterConfigured() async {
+    await Future.delayed(Duration(seconds: 5));
     state = state.copyWith(isUnconfigured: true);
   }
 
