6df71bbb21e8650c1a1cf8bd90de8905de5df5ca
Austin Chang <austin.chang@belkin.com>
2024-06-04 09:44:35 +0800

    Add localization screens

--- test/page/pnp/localizations/pnp_admin_view_test.dart
+++ test/page/pnp/localizations/pnp_admin_view_test.dart
@@ -1,35 +1,81 @@
 import 'dart:convert';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:privacy_gui/core/jnap/models/device_info.dart';
 import 'package:privacy_gui/core/jnap/router_repository.dart';
+import 'package:privacy_gui/core/utils/logger.dart';
 import 'package:privacy_gui/page/pnp/data/pnp_exception.dart';
 import 'package:privacy_gui/page/pnp/data/pnp_provider.dart';
+import './pnp_admin_view_test_mocks.dart' as Mock;
 import 'package:privacy_gui/page/pnp/data/pnp_state.dart';
 import 'package:privacy_gui/page/pnp/pnp_admin_view.dart';
 
+import '../../../common/mock_firebase_messaging.dart';
 import '../../../common/test_responsive_widget.dart';
 import '../../../common/testable_router.dart';
 import '../../../test_data/device_info_test_data.dart';
 
 @GenerateNiceMocks([MockSpec<PnpNotifier>(), MockSpec<RouterRepository>()])
-void main() {
-  late MockPnpNotifier mockPnpNotifier;
+void main() async {
+  late Mock.MockPnpNotifier mockPnpNotifier;
+
+  setupFirebaseMessagingMocks();
+  await Firebase.initializeApp();
 
   setUp(() {
-    mockPnpNotifier = MockPnpNotifier();
-    // when(mockPnpNotifier.build()).thenReturn(PnpState(
-    //     deviceInfo:
-    //         NodeDeviceInfo.fromJson(jsonDecode(testDeviceInfo)['output'])));
-    // when(mockPnpNotifier.checkAdminPassword(null))
-    //     .thenThrow(ExceptionInvalidAdminPassword());
-    // when(mockPnpNotifier.fetchDeviceInfo()).thenAnswer((_) async => {});
-    // when(mockPnpNotifier.checkInternetConnection()).thenAnswer((_) async => {});
-    // when(mockPnpNotifier.checkRouterConfigured()).thenAnswer((_) async => {});
-    // when(mockPnpNotifier.save()).thenAnswer((_) async => {});
+    mockPnpNotifier = Mock.MockPnpNotifier();
+    when(mockPnpNotifier.build()).thenReturn(PnpState(
+        deviceInfo:
+            NodeDeviceInfo.fromJson(jsonDecode(testDeviceInfo)['output']),
+        isUnconfigured: true));
+    when(mockPnpNotifier.checkAdminPassword(null)).thenAnswer((_) {
+      throw ExceptionInvalidAdminPassword();
+    });
+    // .thenThrow(ExceptionInvalidAdminPassword());
+    when(mockPnpNotifier.fetchDeviceInfo()).thenAnswer((_) async {});
+    when(mockPnpNotifier.checkInternetConnection()).thenAnswer((_) async {});
+    when(mockPnpNotifier.checkRouterConfigured()).thenAnswer((_) async {});
+    when(mockPnpNotifier.save()).thenAnswer((_) async {});
   });
+
+  testLocalizations('pnp admin view - checking internet ',
+      (tester, locale) async {
+    when(mockPnpNotifier.checkInternetConnection()).thenAnswer((_) async {
+      await Future.delayed(const Duration(seconds: 3));
+    });
+
+    await tester.pumpWidget(
+      testableSingleRoute(
+        child: const PnpAdminView(),
+        locale: locale,
+        overrides: [pnpProvider.overrideWith(() => mockPnpNotifier)],
+      ),
+    );
+    await tester.pump(const Duration(seconds: 1));
+  });
+
+  testLocalizations('pnp admin view - internet checked ',
+      (tester, locale) async {
+    when(mockPnpNotifier.checkInternetConnection()).thenAnswer((_) async {
+      await Future.delayed(const Duration(seconds: 3));
+    });
+    when(mockPnpNotifier.checkRouterConfigured()).thenAnswer((_) async {
+      await Future.delayed(const Duration(seconds: 3));
+    });
+
+    await tester.pumpWidget(
+      testableSingleRoute(
+        child: const PnpAdminView(),
+        locale: locale,
+        overrides: [pnpProvider.overrideWith(() => mockPnpNotifier)],
+      ),
+    );
+    await tester.pump(const Duration(seconds: 3));
+  });
+
   testLocalizations('pnp admin view - input router password ',
       (tester, locale) async {
     await tester.pumpWidget(
@@ -39,31 +85,45 @@ void main() {
         overrides: [pnpProvider.overrideWith(() => mockPnpNotifier)],
       ),
     );
-    await tester.pump(const Duration(seconds: 6));
-  });
-  testLocalizations('pnp admin view - checking internet ',
-      (tester, locale) async {
-    await tester.pumpWidget(
-      testableSingleRoute(
-        child: const PnpAdminView(),
-        locale: locale,
-        overrides: [pnpProvider.overrideWith(() => TestMockPnpNotifier())],
-      ),
-    );
+    await tester.pumpAndSettle();
   });
 
-  testLocalizations('pnp admin view - internet connected',
-      (tester, locale) async {
+  testLocalizations('pnp admin view - factory reset', (tester, locale) async {
+    when(mockPnpNotifier.checkRouterConfigured())
+        .thenThrow(ExceptionRouterUnconfigured());
     await tester.pumpWidget(
       testableSingleRoute(
         child: const PnpAdminView(),
         locale: locale,
-        overrides: [pnpProvider.overrideWith(() => TestMockPnpNotifier())],
+        overrides: [pnpProvider.overrideWith(() => mockPnpNotifier)],
       ),
     );
     await tester.pumpAndSettle();
-    await tester.pump(Duration(seconds: 1));
   });
+
+  // testLocalizations('pnp admin view - checking internet ',
+  //     (tester, locale) async {
+  //   await tester.pumpWidget(
+  //     testableSingleRoute(
+  //       child: const PnpAdminView(),
+  //       locale: locale,
+  //       overrides: [pnpProvider.overrideWith(() => TestMockPnpNotifier())],
+  //     ),
+  //   );
+  // });
+
+  // testLocalizations('pnp admin view - internet connected',
+  //     (tester, locale) async {
+  //   await tester.pumpWidget(
+  //     testableSingleRoute(
+  //       child: const PnpAdminView(),
+  //       locale: locale,
+  //       overrides: [pnpProvider.overrideWith(() => TestMockPnpNotifier())],
+  //     ),
+  //   );
+  //   await tester.pumpAndSettle();
+  //   await tester.pump(Duration(seconds: 1));
+  // });
 }
 
 class TestMockPnpNotifier extends BasePnpNotifier {
