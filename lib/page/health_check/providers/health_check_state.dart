791339aeff8053b5420175e165fd9bd9f0618e31
Peter Jhong <peter.jhong@linksys.com>
2024-06-21 12:11:13 +0800

    [Speed Test] Apply new UI, adjust animattion meter, and fix the flow

--- lib/page/health_check/providers/health_check_state.dart
+++ lib/page/health_check/providers/health_check_state.dart
@@ -1,31 +1,75 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
 import 'package:equatable/equatable.dart';
+
 import 'package:privacy_gui/core/jnap/models/health_check_result.dart';
 import 'package:privacy_gui/core/jnap/result/jnap_result.dart';
 
 class HealthCheckState extends Equatable {
   final String step;
+  final String? timestamp;
   final List<HealthCheckResult> result;
   final JNAPError? error;
 
-  @override
-  List<Object?> get props => [step, result, error];
+  const HealthCheckState({
+    this.step = 'latency',
+    this.timestamp,
+    this.result = const [],
+    this.error,
+  });
 
-  const HealthCheckState(
-      {required this.step, required this.result, this.error});
+  @override
+  List<Object?> get props => [step, timestamp, result, error];
 
   factory HealthCheckState.init() {
-    return const HealthCheckState(step: '', result: [], error: null);
+    return const HealthCheckState(
+      step: 'latency',
+      result: [],
+    );
   }
 
-  factory HealthCheckState.reset() {
-    return const HealthCheckState(step: '', result: [], error: null);
-  }
-
-  HealthCheckState copyWith(
-      {String? step, List<HealthCheckResult>? result, JNAPError? error}) {
+  HealthCheckState copyWith({
+    String? step,
+    String? timestamp,
+    List<HealthCheckResult>? result,
+    JNAPError? error,
+  }) {
     return HealthCheckState(
-        step: step ?? this.step,
-        result: result ?? this.result,
-        error: error ?? this.error);
+      step: step ?? this.step,
+      timestamp: timestamp ?? this.timestamp,
+      result: result ?? this.result,
+      error: error ?? this.error,
+    );
   }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'step': step,
+      'timestamp': timestamp,
+      'result': result.map((x) => x.toJson()).toList(),
+      'error': error,
+    }..removeWhere((key, value) => value == null);
+  }
+
+  factory HealthCheckState.fromMap(Map<String, dynamic> map) {
+    return HealthCheckState(
+      step: map['step'] as String,
+      timestamp: map['timestamp'] != null ? map['timestamp'] as String : null,
+      result: List<HealthCheckResult>.from(
+        map['result'].map<HealthCheckResult>(
+          (x) => HealthCheckResult.fromJson(x as Map<String, dynamic>),
+        ),
+      ),
+      error: map['error']
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory HealthCheckState.fromJson(String source) =>
+      HealthCheckState.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  bool get stringify => true;
 }
