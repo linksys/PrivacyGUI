39a25e5c9f496d6a8295df508a6636716d0dc629
Hank Yu <hank.yu@belkin.com>
2024-07-19 17:45:37 +0800

    [Screenshot Testing]
    Added screenshot testing for all pages of Local Network Settings feature

---
+++ test/page/advanced_settings/local_network_settings/views/localizations/dhcp_reservations_edit_view_test.dart
@@ -0,0 +1,40 @@
+import 'package:mockito/annotations.dart';
+import 'package:privacy_gui/page/advanced_settings/_advanced_settings.dart';
+import 'package:privacy_gui/route/route_model.dart';
+import '../../../../../common/test_responsive_widget.dart';
+import '../../../../../common/testable_router.dart';
+import '../../../../../test_data/local_network_settings_state.dart';
+
+@GenerateNiceMocks([MockSpec<LocalNetworkSettingsNotifier>()])
+void main() {
+  testLocalizations('DHCP reservations edit view test - Add DHCP reservation',
+      (tester, locale) async {
+
+    final widget = testableSingleRoute(
+      config: LinksysRouteConfig(column: ColumnGrid(column: 9)),
+      locale: locale,
+      child: const DHCPReservationsEditView(
+        args: {'viewType': 'add'},
+      ),
+    );
+    await tester.pumpWidget(widget);
+  });
+
+  testLocalizations('DHCP reservations edit view test - Edit DHCP reservation',
+      (tester, locale) async {
+    final mockState =
+        LocalNetworkSettingsState.fromMap(mocklocalNetworkSettingsState);
+
+    final widget = testableSingleRoute(
+      config: LinksysRouteConfig(column: ColumnGrid(column: 9)),
+      locale: locale,
+      child: DHCPReservationsEditView(
+        args: {
+          'viewType': 'edit',
+          'item': mockState.dhcpReservationList[1],
+        },
+      ),
+    );
+    await tester.pumpWidget(widget);
+  });
+}
---
+++ test/test_data/local_network_settings_state.dart
@@ -0,0 +1,31 @@
+final mocklocalNetworkSettingsState = {
+  "hostName": "Linksys00062",
+  "ipAddress": "10.11.1.1",
+  "subnetMask": "255.255.255.0",
+  "isDHCPEnabled": true,
+  "firstIPAddress": "10.11.1.10",
+  "lastIPAddress": "10.11.1.254",
+  "maxUserLimit": 245,
+  "maxUserAllowed": 245,
+  "clientLeaseTime": 1440,
+  "minAllowDHCPLeaseMinutes": 1,
+  "maxAllowDHCPLeaseMinutes": 525600,
+  "minNetworkPrefixLength": 16,
+  "maxNetworkPrefixLength": 30,
+  "dns1": "168.95.1.1",
+  "dns2": "168.95.192.1",
+  "dns3": "8.8.8.8",
+  "wins": null,
+  "dhcpReservationList": [
+    {
+      "macAddress": "A4:83:E7:36:4C:22",
+      "ipAddress": "10.11.1.39",
+      "description": "ASTWP-028292"
+    },
+    {
+      "macAddress": "A4:83:E7:36:4C:33",
+      "ipAddress": "10.11.1.33",
+      "description": "Test-iPhone-15-Pro"
+    }
+  ]
+};
