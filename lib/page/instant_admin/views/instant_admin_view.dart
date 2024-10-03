71d0031c432cb9086e89a8b8a48eb7a566ae1d5b
Austin Chang <austin.chang@belkin.com>
2024-10-03 09:53:40 +0800

    - testing and remove hide manual fw update condition

--- lib/page/instant_admin/views/instant_admin_view.dart
+++ lib/page/instant_admin/views/instant_admin_view.dart
@@ -181,29 +181,50 @@ class _NetworkAdminViewState extends ConsumerState<NetworkAdminView> {
               },
             ),
           ],
-          if (dashboardManager.skuModelNumber?.endsWith('AH') != true) ...[
-            const AppGap.small2(),
-            AppListCard(
-              title: AppText.bodyLarge(loc(context).manualFirmwareUpdate),
-              onTap: () async {
-                final result = await FilePicker.platform.pickFiles();
-                if (result != null) {
-                  final file = result.files.single;
-                  logger.d('XXXXX: Manual Firmware update: file: ${file.name}');
-                  ref
-                      .read(firmwareUpdateProvider.notifier)
-                      .manualFirmwareUpdate(file.name, file.bytes ?? [])
-                      .then((value) {
-                    if (value) {
-                      showSuccessSnackBar(context, 'Firmware update success');
-                    } else {
-                      showFailedSnackBar(context, 'Error updating firmware');
-                    }
-                  });
-                }
-              },
-            ),
-          ],
+          // if (dashboardManager.skuModelNumber?.endsWith('AH') != true) ...[
+          //   const AppGap.small2(),
+          //   AppListCard(
+          //     title: AppText.bodyLarge(loc(context).manualFirmwareUpdate),
+          //     onTap: () async {
+          //       final result = await FilePicker.platform.pickFiles();
+          //       if (result != null) {
+          //         final file = result.files.single;
+          //         logger.d('XXXXX: Manual Firmware update: file: ${file.name}');
+          //         ref
+          //             .read(firmwareUpdateProvider.notifier)
+          //             .manualFirmwareUpdate(file.name, file.bytes ?? [])
+          //             .then((value) {
+          //           if (value) {
+          //             showSuccessSnackBar(context, 'Firmware update success');
+          //           } else {
+          //             showFailedSnackBar(context, 'Error updating firmware');
+          //           }
+          //         });
+          //       }
+          //     },
+          //   ),
+          // ],
+          const AppGap.small2(),
+          AppListCard(
+            title: AppText.bodyLarge(loc(context).manualFirmwareUpdate),
+            onTap: () async {
+              final result = await FilePicker.platform.pickFiles();
+              if (result != null) {
+                final file = result.files.single;
+                logger.d('XXXXX: Manual Firmware update: file: ${file.name}');
+                ref
+                    .read(firmwareUpdateProvider.notifier)
+                    .manualFirmwareUpdate(file.name, file.bytes ?? [])
+                    .then((value) {
+                  if (value) {
+                    showSuccessSnackBar(context, 'Firmware update success');
+                  } else {
+                    showFailedSnackBar(context, 'Error updating firmware');
+                  }
+                });
+              }
+            },
+          ),
         ],
       ),
     );
