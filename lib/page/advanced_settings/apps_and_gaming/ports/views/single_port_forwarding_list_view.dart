093f8571e608e1f2da31ea8fa22d893eb9096a02
Austin Chang <austin.chang@belkin.com>
2024-10-30 11:30:47 +0800

    - update single port forwarding

--- lib/page/advanced_settings/apps_and_gaming/ports/views/single_port_forwarding_list_view.dart
+++ lib/page/advanced_settings/apps_and_gaming/ports/views/single_port_forwarding_list_view.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:privacy_gui/core/jnap/models/single_port_forwarding_rule.dart';
 import 'package:privacy_gui/localization/localization_hook.dart';
 import 'package:privacy_gui/page/advanced_settings/apps_and_gaming/ports/_ports.dart';
 import 'package:privacy_gui/page/advanced_settings/apps_and_gaming/ports/views/widgets/_widgets.dart';
 import 'package:privacy_gui/page/components/shortcuts/dialogs.dart';
+import 'package:privacy_gui/page/components/styled/consts.dart';
 
 import 'package:privacy_gui/page/components/styled/styled_page_view.dart';
 import 'package:privacy_gui/page/components/views/arguments_view.dart';
 import 'package:privacy_gui/route/constants.dart';
 import 'package:privacygui_widgets/widgets/_widgets.dart';
+import 'package:privacygui_widgets/widgets/card/card.dart';
+import 'package:privacygui_widgets/widgets/card/setting_card.dart';
+import 'package:privacygui_widgets/widgets/container/responsive_layout.dart';
+import 'package:privacygui_widgets/widgets/dropdown/dropdown_button.dart';
+import 'package:privacygui_widgets/widgets/gap/const/spacing.dart';
+import 'package:privacygui_widgets/widgets/input_field/ip_form_field.dart';
 import 'package:privacygui_widgets/widgets/page/layout/basic_layout.dart';
+import 'package:privacygui_widgets/widgets/table/editable_table_settings_view.dart';
 
 class SinglePortForwardingListView extends ArgumentsConsumerStatelessView {
   const SinglePortForwardingListView({super.key, super.args});
@@ -36,9 +45,13 @@ class _SinglePortForwardingContentViewState
     extends ConsumerState<SinglePortForwardingListContentView> {
   late final SinglePortForwardingListNotifier _notifier;
 
+  late final TextEditingController applicationTextController;
+  bool _isEditingValid = true;
+
   @override
   void initState() {
     _notifier = ref.read(singlePortForwardingListProvider.notifier);
+    applicationTextController = TextEditingController();
     doSomethingWithSpinner(
       context,
       _notifier.fetch(),
@@ -55,9 +68,18 @@ class _SinglePortForwardingContentViewState
   @override
   Widget build(BuildContext context) {
     final state = ref.watch(singlePortForwardingListProvider);
+    final submaskToken = ref
+        .read(singlePortForwardingRuleProvider.notifier)
+        .subnetMask
+        .split('.');
+    final prefixIP =
+        ref.read(singlePortForwardingRuleProvider.notifier).ipAddress;
     return StyledAppPageView(
       title: loc(context).singlePortForwarding,
       scrollable: true,
+      useMainPadding: false,
+      appBarStyle: AppBarStyle.none,
+      padding: EdgeInsets.zero,
       child: AppBasicLayout(
         content: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -110,6 +132,154 @@ class _SinglePortForwardingContentViewState
                 if (state.rules.isEmpty) const EmptyRuleCard(),
               ],
             ),
+            const AppGap.medium(),
+            AppEditableTableSettingsView<SinglePortForwardingRule>(
+              title: loc(context).singlePortForwarding,
+              headers: [
+                'Application Name',
+                loc(context).externalPort,
+                loc(context).internalPort,
+                'Protocol',
+                'Device IP#',
+              ],
+              actionHeader: loc(context).action,
+              columnWidths: ResponsiveLayout.isOverLargeLayout(context)
+                  ? const {
+                      0: FractionColumnWidth(.25),
+                      1: FractionColumnWidth(.08),
+                      2: FractionColumnWidth(.08),
+                      3: FractionColumnWidth(.18),
+                      4: FractionColumnWidth(.27),
+                    }
+                  : const {
+                      0: FractionColumnWidth(.2),
+                      1: FractionColumnWidth(.08),
+                      2: FractionColumnWidth(.08),
+                      3: FractionColumnWidth(.2),
+                      4: FractionColumnWidth(.3),
+                    },
+              dataList: [...state.rules],
+              editRowIndex: 0,
+              cellBuilder: (context, index, rule) {
+                return switch (index) {
+                  0 => AppText.bodyLarge(rule.description),
+                  1 => AppText.bodyLarge('${rule.externalPort}'),
+                  2 => AppText.bodyLarge('${rule.internalPort}'),
+                  3 =>
+                    AppText.bodyLarge(getProtocolTitle(context, rule.protocol)),
+                  4 => AppText.bodyLarge(rule.internalServerIPAddress),
+                  _ => AppText.bodyLarge(''),
+                };
+              },
+              editCellBuilder: (context, index, rule) {
+                return switch (index) {
+                  0 => AppTextField.outline(
+                      controller: applicationTextController,
+                      onChanged: (value) {
+                        setState(() {
+                          _isEditingValid = value.isNotEmpty;
+                        });
+                      },
+                    ),
+                  1 => AppTextField.minMaxNumber(
+                      min: 0,
+                      max: 65535,
+                      border: OutlineInputBorder(),
+                      controller: TextEditingController()
+                        ..text = '${rule.externalPort}'),
+                  2 => AppTextField.minMaxNumber(
+                      min: 0,
+                      max: 65535,
+                      border: OutlineInputBorder(),
+                      controller: TextEditingController()
+                        ..text = '${rule.internalPort}'),
+                  3 => AppDropdownButton(
+                      initial: rule.protocol,
+                      items: const ['TCP', 'UDP', 'Both'],
+                      label: (e) => getProtocolTitle(context, e),
+                      onChanged: (value) {},
+                    ),
+                  4 => AppIPFormField(
+                      controller: TextEditingController()
+                        ..text = rule.internalServerIPAddress,
+                      border: const OutlineInputBorder(),
+                      octet1ReadOnly: submaskToken[0] == '255',
+                      octet2ReadOnly: submaskToken[1] == '255',
+                      octet3ReadOnly: submaskToken[2] == '255',
+                      octet4ReadOnly: submaskToken[3] == '255',
+                    ),
+                  _ => AppText.bodyLarge(''),
+                };
+              },
+              createNewItem: () => SinglePortForwardingRule(
+                  isEnabled: true,
+                  externalPort: 0,
+                  protocol: 'Both',
+                  internalServerIPAddress: prefixIP,
+                  internalPort: 0,
+                  description: ''),
+              isEditingDataValid: _isEditingValid,
+              itemCardBuilder: (context, rule) => EditableListItem(
+                  title: rule.description,
+                  content: Table(
+                    children: [
+                      TableRow(
+                        children: [
+                          AppSettingCard.noBorder(
+                            padding: EdgeInsets.only(bottom: Spacing.medium),
+                            title: loc(context).internalPort,
+                            description: '${rule.internalPort}',
+                          ),
+                          AppSettingCard.noBorder(
+                            padding: EdgeInsets.only(bottom: Spacing.medium),
+                            title: loc(context).externalPort,
+                            description: '${rule.internalPort}',
+                          )
+                        ],
+                      ),
+                      TableRow(
+                        children: [
+                          AppSettingCard.noBorder(
+                            padding: EdgeInsets.only(bottom: Spacing.medium),
+                            title: loc(context).protocol,
+                            description:
+                                getProtocolTitle(context, rule.protocol),
+                          ),
+                          AppSettingCard.noBorder(
+                            padding: EdgeInsets.only(bottom: Spacing.medium),
+                            title: loc(context).ipAddress,
+                            description: rule.internalServerIPAddress,
+                          )
+                        ],
+                      ),
+                    ],
+                  )),
+              editItemCardBuilder: (context, rule) {
+                return AppCard(
+                    child: Column(
+                  children: [
+                    AppTextField.outline(
+                      controller: applicationTextController,
+                    ),
+                    AppDropdownButton(
+                      initial: rule.protocol,
+                      items: const ['TCP', 'UDP', 'Both'],
+                      label: (e) => getProtocolTitle(context, e),
+                      onChanged: (value) {},
+                    ),
+                    AppIPFormField(
+                      controller: TextEditingController()
+                        ..text = rule.internalServerIPAddress,
+                      border: const OutlineInputBorder(),
+                      octet1ReadOnly: submaskToken[0] == '255',
+                      octet2ReadOnly: submaskToken[1] == '255',
+                      octet3ReadOnly: submaskToken[2] == '255',
+                      octet4ReadOnly: submaskToken[3] == '255',
+                    ),
+                  ],
+                ));
+              },
+            )
           ],
         ),
       ),
