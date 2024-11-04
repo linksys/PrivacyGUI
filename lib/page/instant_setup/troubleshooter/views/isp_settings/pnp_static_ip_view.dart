d2ef610b9f981cc316551a9ff1c39e69f49e3077
AustinChangLinksys <79675086+AustinChangLinksys@users.noreply.github.com>
2024-11-04 13:11:39 +0800

    - handle unexcept error on pnp auth (#180)

--- lib/page/instant_setup/troubleshooter/views/isp_settings/pnp_static_ip_view.dart
+++ lib/page/instant_setup/troubleshooter/views/isp_settings/pnp_static_ip_view.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'package:privacy_gui/core/jnap/router_repository.dart';
 import 'package:privacy_gui/core/utils/logger.dart';
 import 'package:privacy_gui/localization/localization_hook.dart';
 import 'package:privacy_gui/page/advanced_settings/internet_settings/providers/_providers.dart';
 import 'package:privacy_gui/page/components/styled/styled_page_view.dart';
-import 'package:privacy_gui/page/instant_setup/data/pnp_provider.dart';
 import 'package:privacy_gui/route/constants.dart';
 import 'package:privacy_gui/utils.dart';
 import 'package:privacygui_widgets/widgets/gap/const/spacing.dart';
