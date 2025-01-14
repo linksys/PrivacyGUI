0ccf8e8762b159028f5683da5bca87a563b651cd
Hank Yu <hank.yu@belkin.com>
2025-01-14 13:46:13 +0800

    [NOW-374]
    Added screenshot testing cases for forgetPassword feature

---
+++ test/page/login/localizations/local_router_recovery_view_test.dart
@@ -0,0 +1,94 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:privacy_gui/page/instant_admin/_instant_admin.dart';
+import 'package:privacy_gui/page/login/views/local_router_recovery_view.dart';
+import '../../../common/test_responsive_widget.dart';
+import '../../../common/testable_router.dart';
+import '../../../mocks/_index.dart';
+import '../../../test_data/_index.dart';
+
+void main() {
+  late MockRouterPasswordNotifier mockRouterPasswordNotifier;
+
+  setUp(() {
+    mockRouterPasswordNotifier = MockRouterPasswordNotifier();
+  });
+
+  testLocalizations('local router recovery view - init state',
+      (tester, locale) async {
+    when(mockRouterPasswordNotifier.build())
+        .thenReturn(RouterPasswordState.fromMap(routerPasswordTestState1));
+
+    await tester.pumpWidget(
+      testableSingleRoute(
+        child: const LocalRouterRecoveryView(),
+        locale: locale,
+        overrides: [
+          routerPasswordProvider.overrideWith(() => mockRouterPasswordNotifier),
+        ],
+      ),
+    );
+
+    await tester.pump(Duration(seconds: 1));
+  });
+
+  testLocalizations('local router recovery view - wrong input code',
+      (tester, locale) async {
+    when(mockRouterPasswordNotifier.build()).thenReturn(
+      RouterPasswordState.fromMap(routerPasswordTestState1)
+          .copyWith(remainingErrorAttempts: 2),
+    );
+
+    await tester.pumpWidget(
+      testableSingleRoute(
+        child: const LocalRouterRecoveryView(),
+        locale: locale,
+        overrides: [
+          routerPasswordProvider.overrideWith(() => mockRouterPasswordNotifier),
+        ],
+      ),
+    );
+
+    await tester.pump(Duration(seconds: 1));
+  });
+
+  testLocalizations('local router recovery view - last chance',
+      (tester, locale) async {
+    when(mockRouterPasswordNotifier.build()).thenReturn(
+      RouterPasswordState.fromMap(routerPasswordTestState1)
+          .copyWith(remainingErrorAttempts: 1),
+    );
+
+    await tester.pumpWidget(
+      testableSingleRoute(
+        child: const LocalRouterRecoveryView(),
+        locale: locale,
+        overrides: [
+          routerPasswordProvider.overrideWith(() => mockRouterPasswordNotifier),
+        ],
+      ),
+    );
+
+    await tester.pump(Duration(seconds: 1));
+  });
+
+  testLocalizations('local router recovery view - being locked',
+      (tester, locale) async {
+    when(mockRouterPasswordNotifier.build()).thenReturn(
+      RouterPasswordState.fromMap(routerPasswordTestState1)
+          .copyWith(remainingErrorAttempts: 0),
+    );
+
+    await tester.pumpWidget(
+      testableSingleRoute(
+        child: const LocalRouterRecoveryView(),
+        locale: locale,
+        overrides: [
+          routerPasswordProvider.overrideWith(() => mockRouterPasswordNotifier),
+        ],
+      ),
+    );
+
+    await tester.pump(Duration(seconds: 1));
+  });
+}
