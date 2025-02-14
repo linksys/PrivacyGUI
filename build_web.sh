b07d8ad36f22e0a78ebc983d6529664b247de6bd
Austin Chang <austin.chang@belkin.com>
2025-02-14 12:53:08 +0800

    temp commit

--- build_web.sh
+++ build_web.sh
@@ -7,7 +7,7 @@ function buildWebApp() {
   else
     flutter build web --target=lib/main.dart --base-href="/${href}" --build-number="${buildNumber}" --dart-define=force="${force}" --dart-define=cloud_env="${cloud}" --dart-define=enable_env_picker="${picker}" --dart-define=ca="${ca}" --web-renderer html --no-tree-shake-icons
   fi
-  rm -rf ./build/web/canvasKit  
+  # rm -rf ./build/web/canvasKit  
 }
 
 buildNumber=$1
