677a041bbbac736e78dcafd71e7fc6aeebec84ec
Austin Chang <austin.chang@belkin.com>
2025-02-18 10:49:03 +0800

    - update fontFallbackBaseUrl and canvasKitBaseUrl

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -19,8 +19,8 @@ _flutter.buildConfig = { "engineRevision": "cb4b5fff73850b2e42bd4de7cb9a4310a78a
 
 _flutter.loader.load({
   config: {
-    fontFallbackBaseUrl: "/assets/",
-    canvasKitBaseUrl: "/assets/"
+    fontFallbackBaseUrl: "./assets/",
+    canvasKitBaseUrl: "./assets/"
   },
   serviceWorkerSettings: {
     serviceWorkerVersion: "689878468"
