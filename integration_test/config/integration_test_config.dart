a7e058f166eb04c3f61c207e0e581db8bbdf2995
Peter Jhong <peter.jhong@linksys.com>
2025-03-28 17:13:42 +0800

    [Integration Test] Implement the test for wifi name and password

--- integration_test/config/integration_test_config.dart
+++ integration_test/config/integration_test_config.dart
@@ -1,7 +1,12 @@
 class IntegrationTestConfig {
-  static const String password = String.fromEnvironment('password', defaultValue: 'Linksys123!');
+  static const String password =
+      String.fromEnvironment('password', defaultValue: 'Linksys123!');
   static const String recoveryCode =
       String.fromEnvironment('recoveryCode', defaultValue: '00000');
+  static const String newPassword =
+      String.fromEnvironment('newPassword', defaultValue: 'Linksys123!');
   static const String passwordHint =
       String.fromEnvironment('passwordHint', defaultValue: 'Password hint');
+  static const String wifiBands =
+      String.fromEnvironment('wifiBands', defaultValue: '2.4,5,guest');
 }
