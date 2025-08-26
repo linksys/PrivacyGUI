d756bcca2acc38445aedc725364c4cba42f7dddd
Austin Chang <austin.chang@belkin.com>
2025-08-26 16:16:51 +0800

    - fixes issues on internet_settings_set_3_test.dart - remove extract error message

--- test_scripts/extract_failure_messages.dart
+++ test_scripts/extract_failure_messages.dart
@@ -5,17 +5,17 @@ void main(List<String> args) {
     print('No arguments provided.');
     return;
   }
-  const startMarker = "══╡ EXCEPTION CAUGHT BY FLUTTER TEST FRAMEWORK ╞═════════════════";
-  const endMarker = "end of failure";
-  const stackMarker = ", this was the stack:";
+  // const startMarker = "══╡ EXCEPTION CAUGHT BY FLUTTER TEST FRAMEWORK ╞═════════════════";
+  // const endMarker = "end of failure";
+  // const stackMarker = ", this was the stack:";
   final text = args[0];
-  int startIndex = text.indexOf(startMarker);
-  startIndex = startIndex == -1 ? 0 : startIndex;
-  int endIndex = text.indexOf(endMarker);
-  endIndex = endIndex == -1 ? text.length : endIndex;
-  int stackIndex = text.indexOf(stackMarker);
-  stackIndex = stackIndex == -1 ? endIndex : stackIndex;
-  String result = text.substring(startIndex, stackIndex);
-  result = result.replaceAll(RegExp(r'\n'), '<br>');
+  // int startIndex = text.indexOf(startMarker);
+  // startIndex = startIndex == -1 ? 0 : startIndex;
+  // int endIndex = text.indexOf(endMarker);
+  // endIndex = endIndex == -1 ? text.length : endIndex;
+  // int stackIndex = text.indexOf(stackMarker);
+  // stackIndex = stackIndex == -1 ? endIndex : stackIndex;
+  // String result = text.substring(startIndex, stackIndex);
+  final result = text.replaceAll(RegExp(r'\n'), '<br>');
   print(result);
 }
